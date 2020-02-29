f1b56ea95264dcec205b96f7e1f5fbafd14360a5
Konstantinos Biris <sc19kb@leeds.ac.uk>
2020-02-29 17:15:19 +0000

    Added first stubs for template.c functions

--- cw1/template.c
+++ cw1/template.c
@@ -4,3 +4,29 @@
 #include <strin.h>
 #include "book_management.h"
 #include "users.h"
+
+unsigned int NumberUsers; //The total number of users in the library.
+unsigned int UsersIdCounter; //a counter for creating unique user IDs.
+
+User* AllUsers[MaxUserID]; //An array containing all the users in the library.
+
+void register(User* u){
+
+}
+
+void login(){
+
+}
+
+void booksearch(struct Book* b){
+
+}
+
+void borrowBook(struct Book* b){
+
+}
+
+
+void returnBook(struct Book* b){
+
+}
